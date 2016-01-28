37e2a18c0f02addf221c69dd62649a15d2a5a6d5
tomohiroszuki <kk.tm.gjm@gmail.com>
2016-01-28 15:31:31 +0000

    add edit update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def show
    @user = User.find(params[:id])
@@ -18,10 +19,22 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
 
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "ユーザー情報を更新しました。"
+    else 
+      render 'edit'
+    end
+  end
+
+  private
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :area, :university)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
